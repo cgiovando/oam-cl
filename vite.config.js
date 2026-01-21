2576ca71039e87b112234bfbb1d3b1d28ce300a1
cristiano <cristiano@Mac.lan>
2026-01-20 16:46:47 -0800

    chore: update base path for oam-cl deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/oam-fs/', 
+  base: '/oam-cl/', 
   plugins: [react()],
   server: {
     proxy: {
